a52c5874254ce98b889c05d979f4074dd936da85
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-06-06 19:42:26 +0200

    done refactored to strict things up

--- R/bitjson.R
+++ R/bitjson.R
@@ -3,124 +3,78 @@
 #' Is a JSON string a bit array?
 #'
 #' @param json JSON string.
-#' @param compressed Is \code{json} compressed?
 #' @return Logical.
 #'
 #' @export
-isBitJSON <- function(json, compressed=FALSE) {
-  stopifnot(isTruthyChr(json), is.logical(compressed))
-  if (!compressed) {
-    return(grepl('^\\[("?(?:0?0|0?1)"?,)*"?(?:0?0|0?1)"?\\]$', 
-                 json, 
-                 perl=TRUE))
-  } else if (compressed) {
-    return(grepl('^\\[("?\\d+"?,)*"?\\d+"?\\]$', 
-                 json, 
-                 perl=TRUE))
-  }
+isBitJSON <- function(json) {
+  stopifnot(isTruthyChr(json))
+  return(grepl(paste0('(?:^\\[((?:0|1),)*(?:0|1)\\]$)|', 
+                      '(?:^\\[(?:(?:(?:0|1),)*\\d+,(?:0|1),?)+(?:0|1)*\\]$)'), 
+               json, 
+               perl=TRUE))
 }
 
 #' Does a JSON string contain bit json?
 #'
 #' @param json JSON string.
-#' @param compressed Is \code{json} compressed?
 #' @return Logical.
 #'
 #' @export 
-containsBitJSON <- function(json, compressed=FALSE) {
-  stopifnot(isTruthyChr(json), is.logical(compressed))
-  if (!compressed) {
-    return(grepl('^.*\\[("?(?:0?0|0?1)"?,)*"?(?:0?0|0?1)"?\\].*$', 
-                 json, 
-                 perl=TRUE))
-  } else if (compressed) {
-    return(grepl('^.*\\[("?\\d+"?,)*"?\\d+"?\\].*$', 
-                 json, 
-                 perl=TRUE))
-  }
+containsBitJSON <- function(json) {
+  stopifnot(isTruthyChr(json))
+  return(grepl(paste0('(?:^.*\\[((?:0|1),)*(?:0|1)\\].*$)|', 
+                      '(?:^.*\\[(?:(?:(?:0|1),)*\\d+,(?:0|1),?)+(?:0|1)*\\].*$)'), 
+               json, 
+               perl=TRUE))
 }
 
 #' Extract bit JSON from a JSON string
 #'
 #' @param json JSON string.
-#' @param compressed Is \code{json} compressed?
 #' @return JSON string.
 #'
 #' @export 
-extractBitJSON <- function(json, compressed=FALSE) {
-  stopifnot(isTruthyChr(json), is.logical(compressed))
-  if (!compressed) {
-    rtn <- regmatches(json,
-                      gregexpr('\\[(?:("?(?:0?0|0?1)"?,)*"?(?:0?0|0?1)"?)+\\]', 
-                               json, 
-                               perl=TRUE))[[1]]
-    return(structure(rtn, class='json'))
-  } else if (compressed) {
-    rtn <- regmatches(json,
-                      gregexpr('\\[(?:("?\\d+"?,)*"?\\d+"?)+\\]', 
-                               json, 
-                               perl=TRUE))[[1]]
-    return(structure(rtn, class='json'))
-  }
+extractBitJSON <- function(json) {
+  stopifnot(isTruthyChr(json))
+  rex <- paste0('(?:\\[((?:0|1),)*(?:0|1)\\])|', 
+                '(?:\\[(?:(?:(?:0|1),)*\\d+,(?:0|1),?)+(?:0|1)*\\])')
+  rtn <- regmatches(json, gregexpr(rex, json, perl=TRUE))[[1]]
+  # serve
+  return(structure(rtn, class='json'))
 }
 
 #' Serialize an R object to bit JSON
 #' 
 #' @param x Any R object.
 #' @param file File name to which to write bit JSON.
-#' @param remote Remote name to which to PUT bit JSON.
 #' @param compress Compress the bit array to a run length encoded digit array?
 #' @return Bit json.
 #' 
 #' @export
-toBitJSON <- function(x,
-                      file=NULL,
-                      remote=NULL,
-                      compress=TRUE) {
+toBitJSON <- function(x, file=NULL, compress=TRUE) {
   stopifnot(isRData(x) | is.function(x), 
-            is.null(file) | is.null(remote),
             is.null(file) | isTruthyChr(file),
-            is.null(remote) | isValidRemoteName(remote),
             is.logical(compress))
   if (compress) {
     z <- jsonlite::toJSON(serializeToBits(x))
   } else if (!compress) {
     z <- jsonlite::toJSON(serializeToBits(x, compress=FALSE))
   }
-  if (is.null(file) && is.null(remote)) {
+  if (is.null(file)) {
     return(z)
   } else if (is.character(file)) {
     cat(z, file=file, append=TRUE)
-    return(invisible(z))
-  } else if (is.character(remote)) {
-    # curl PUT json to remote
     return(invisible(z))
   }
 }
 
 #' Unserialize an R object encoded as bit JSON
 #' 
-#' @param x Bit JSON, JSON string containing bit JSON or remote name
-#' accessible via http that contains bit JSON or JSON containg bit JSON.
+#' @param x Bit JSON.
 #' @return R object.
 #' 
 #' @export
 fromBitJSON <- function(x) {
-  stopifnot(isValidRemoteName(x) | 
-              ((isBitJSON(x) | isBitJSON(x, compressed=TRUE)) | 
-                  (containsBitJSON(x) | containsBitJSON(x, compressed=TRUE))))
-  if (isBitJSON(x)) {
-    return(unserializeFromBits(as.integer(jsonlite::fromJSON(
-      x
-      ))))
-  } else if (containsBitJSON(x)) {
-    return(unserializeFromBits(as.integer(jsonlite::fromJSON(
-      extractBitJSON(x)
-      ))))
-  } else if (isValidRemoteName(x)) {
-    #resource <- curl GET remote
-    return(unserializeFromBits(as.integer(jsonlite::fromJSON(
-      extractBitJSON(resource)
-    ))))
-  }
+  stopifnot(isBitJSON(x))
+  return(unSerializeFromBits(as.integer(jsonlite::fromJSON(x))))
 }
--- tests/testthat/test_bitjson.R
+++ tests/testthat/test_bitjson.R
@@ -5,45 +5,47 @@ testthat::context('mappings and predicates')
 testthat::test_that('mapping preserves data integrity', {
   # setup
   saka <- data.frame(a=1L:2L, b=c('voo', 'doo'))
+  rectng <- data.frame(a=c(1L:7L), b=rep('harman', 7L))
   # mapped still identical
   testthat::expect_identical(fromBitJSON(toBitJSON(saka)), saka)
+  testthat::expect_identical(fromBitJSON(toBitJSON(rectng)), rectng)
 })
 
 testthat::test_that('predicate functions work', {
   
   # setup
   valid.bits <- toBitJSON(419L)
-  invalid.bits <- '[0,1,0,0,\'0\',1,0,1]'
-  nclosed.bits <- paste0('{"acab":', toBitJSON(1L, compress=FALSE), ',"haha":[419]}')
+  invalid.bits <- '[0,1,0,0,"0",1,0,1]'
+  nclosed.bits <- paste0('{"acab":', toBitJSON(1L), ',"haha":[419]}')
   multi <- paste0('[', 
                   toBitJSON(2L, compress=FALSE), ',', 
                   jsonlite::toJSON(99999L), ',', 
-                  toBitJSON(419L, compress=FALSE), 
+                  toBitJSON(419L), 
                   ']')
-  threesix <- serializeToBits(36L)
-  sakawa <- serializeToBits(list(4L, 1L, 9L))
   
   # match
-  testthat::expect_identical(isBitJSON(valid.bits, compressed=TRUE), 
+  testthat::expect_identical(isBitJSON(valid.bits), 
                              TRUE)
   
   # nomatch
-  testthat::expect_identical(isBitJSON(invalid.bits, compressed=FALSE), 
+  testthat::expect_identical(isBitJSON(invalid.bits), 
                              FALSE)
-  testthat::expect_identical(isBitJSON(nclosed.bits, compressed=FALSE), 
+  testthat::expect_identical(isBitJSON(nclosed.bits), 
                              FALSE)
-  testthat::expect_identical(isBitJSON(multi, compressed=FALSE), FALSE)
+  testthat::expect_identical(isBitJSON(multi), 
+                             FALSE)
   
   # contains
-  testthat::expect_identical(containsBitJSON(nclosed.bits), TRUE)
-  testthat::expect_identical(containsBitJSON(multi), TRUE)
+  testthat::expect_identical(containsBitJSON(nclosed.bits), 
+                             TRUE)
+  testthat::expect_identical(containsBitJSON(multi), 
+                             TRUE)
   
   # extract
-  testthat::expect_identical(isBitJSON(extractBitJSON(nclosed.bits, compressed=FALSE)), 
+  testthat::expect_identical(isBitJSON(extractBitJSON(nclosed.bits)), 
                              TRUE)
-  testthat::expect_identical(sapply(extractBitJSON(multi, compressed=FALSE), 
-                                    isBitJSON, 
-                                    compressed=FALSE,
+  testthat::expect_identical(sapply(extractBitJSON(multi), 
+                                    isBitJSON,
                                     USE.NAMES=FALSE), 
                              c(TRUE, TRUE))
   
